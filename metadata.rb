a34a27702c1b4d821e7d54fa1c4b18fdb2f9e406
Andrei Scopenco <ascopenco@parallels.com>
2016-12-28 20:12:00 +0200

    Bump ver

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'andrey@skopenko.net'
 license          'Apache 2.0'
 description      'Installs/Configures VsFTPD daemon'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.1.0'
+version          '0.1.1'
 
 supports 'amazon'
 supports 'redhat'
